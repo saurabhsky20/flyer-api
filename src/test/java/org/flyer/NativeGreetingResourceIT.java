954ac1f707845182f209337407b9c959e4c2a151
quarkusio <no-reply@quarkus.io>
2021-10-05 03:21:02 +0000

    Initial commit

---
+++ src/test/java/org/flyer/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.flyer;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
